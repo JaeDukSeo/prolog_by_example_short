86e88b38e0f7e813b60da8507082c8fc258938b1
Jae Duk Seo <JaeDukSeo@users.noreply.github.com>
2017-10-25 19:00:47 -0400

    Create 1_Bookstore.pl

---
+++ 1_Bookstore.pl
@@ -0,0 +1,44 @@
+hasBook(a,aa,at,10).
+hasBook(b,bb,bt,20).
+hasBook(c,cc,ct,30).
+hasBook(d,dd,dt,40).
+
+hasBook(c,"Robert",book1,30).
+hasBook(c,"Robert",book2,30).
+hasBook(a,"Robert",book1,30).
+
+
+hasBook(d,dd,"Harry Potter",4).
+lives(tim,toronto).
+lives(kim,seoul).
+
+
+shipping(d,toronto,3).
+shipping(d,seoul,3).
+
+
+
+
+
+/*
+	1. Question 
+    
+    hasBook(BS,A,"Harry Potter",LP),
+  shipping(BS,C1,Cost1),
+  shipping(BS,C2,Cost2),
+  lives(P1,C1),
+  lives(P2,C2),
+  P1\=P2,C1\=C2,
+  write("Book store "),write(BS),nl,nl.
+
+*/
+
+/*
+	
+    2. Second question
+    
+    hasBook(BS,"Robert",T,_),
+not( (hasBook(BS2,"Robert",T,_), BS \= BS2)),
+write("The book : "),write(T),write(" Only at one store"),nl.
+	
+*/
